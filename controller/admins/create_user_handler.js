b85631a8d616e0b4476cecbdb6e95a6344915906
lamhai1401 <lamthanhhai141@gmail.com>
2018-06-21 16:53:59 +0700

    fix api create user

--- controller/admins/create_user_handler.js
+++ controller/admins/create_user_handler.js
@@ -1,6 +1,5 @@
 const validate    = require('validate.js');
 const create_user = require('../../services/users/create_user');
-const string      = require('../../util/string');
 const hash        = require('../../util/hash').hash;
 
 const constraints = {
@@ -28,12 +27,12 @@ module.exports = (req, res, next) => {
   };
 
   let err = validate(object, constraints);
-  if (err) return res.json(err);
+  if (err) return res.responseError("USER_CREATED_FAILED", err)
 
   // hash password
   hash(object.password).then(hash_pass => {
     object.password = hash_pass;
-    return create_user(object)
+    return create_user(object);
   }).then(user => {
     res.responseSuccess({success: true, data: user});
   }).catch(err => {
--- services/users/create_user.js
+++ services/users/create_user.js
@@ -1,14 +1,14 @@
 const users_model     = require('../../models/users');
-const get_user        = require('./get_user');
+const user_model      = require('../../models/users');
 const role_services   = require('../../services/roles');
 const user_role_model = require('../../models/user_role');
 
-module.exports = (object) => {
-  return get_user({email: object.email})
+module.exports = (object) => {  
+  return user_model.findOne({email: object.email})
     .then(user => {
       if(user) return Promise.reject('Try to use another mail');
       // check role_type is exist or not and not admin type
-      return role_services.get_role({type: object.role_type});
+      return role_services.get_role_from_type({type: object.role_type});
     }).then(role => {
       if(!role || role.status == 0) {
         return Promise.reject('Invalid/Disable role type');
