dfbfcc065441af1e193b2f0ee413730abf9abe93
lamhai1401 <lamthanhhai141@gmail.com>
2018-07-05 15:55:55 +0700

    a

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const request = require('request');
 const crypto = require('crypto');
-
-const sign = crypto.createHmac('sha256', 'R244m0Fcs72OtwVc0ecQVtPbwqas2nu6I1MCLYQ5fr8=');
+const hash    = require('crypto').createHmac('sha256', secret);
 
 // request('https://cs-portal.zendesk.com/api/v2/tickets/1/audits.json?email=lamthanhhai141@gmail.com&password=123456', { json: true }, (err, res, body) => {
 //   if (err) { return console.log(err); }
