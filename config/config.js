bc7cc9ac2539d2d6eb15fdefe835761015551392
lamhai1401 <lamthanhhai141@gmail.com>
2018-07-09 12:33:03 +0700

    remove .env/ dev mode

--- config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    port                : process.env.PORT,
-    mongodb_url         : process.env.MONGODB_URL,
-    host                : process.env.HOST
-};
