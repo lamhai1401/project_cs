d1feb28a30bca2e56fc7b6e68ac941bd25cb59c2
lamhai1401 <lamthanhhai141@gmail.com>
2018-07-12 16:07:59 +0700

    fix api get account detail

--- controller/users/get_account_detail.js
+++ controller/users/get_account_detail.js
@@ -43,21 +43,22 @@ module.exports = (req, res, next) => {
     };
 
     return request(options, (error, response, body) => {
-      
       if (error) return res.responseError("GET_ACCOUNT_DETAIL_FAILED", error);
       if(body.error) return res.responseError("GET_ACCOUNT_DETAIL_FAILED", body.error_description);
+      console.log(body);
       // recording support activites to data base 
       const account = {
-        email: body.email,
-        name: body.kyc_detail.first_name + body.kyc_detail.last_name,
+        email: body.email || "",
+        name: "",
         google_auth_status: body.enable_google_2fa,
         account_status: body.account_status,
-        kyc_status: body.kyc_status,
+        kyc_status: body.kyc_status || null,
+        kyc_detail: body.kyc_detail || null,
         withdrawal_status: body.withdrawal_status,
         sms_status: false,
-        kyc_detail: body.kyc_detail
       };
       if (body.phone) account.sms_status = true;
+      if (body.kyc_detail) account.name = body.kyc_detail.first_name + body.kyc_detail.last_name;
       return res.responseSuccess({success: true, data: account});
     });
   })
