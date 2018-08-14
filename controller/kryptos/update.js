45700601f2af181cd551ad04b951762267b36bed
lamhai1401 <lamthanhhai141@gmail.com>
2018-08-14 11:49:14 +0700

    fix api request

--- controller/kryptos/update.js
+++ controller/kryptos/update.js
@@ -27,7 +27,6 @@ function kyc_status(req, res, next) {
   // send request to kryptos server
   kryptos.update_kyc_status(object)
   .then(body => {
-    console.log(body);
     return res.responseSuccess({success: true, data: body.message});
   })
   .catch(err => {
