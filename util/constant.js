6dcb6c059aeddc4042efd8059e441bfebd7c69d4
lamhai1401 <lamthanhhai141@gmail.com>
2018-08-14 12:38:28 +0700

    fix url krypto

--- util/constant.js
+++ util/constant.js
@@ -4,7 +4,7 @@ const KRYPTONO_URL = {
   "LOGIN"           : "https://xapi.kryptono.exchange/portal/j_spring_security_check",
   "LOGIN_HISTORIES" : "https://xapi.kryptono.exchange/k/cs/login-history",
   "ACCOUNT_DETAIL"  : "https://xapi.kryptono.exchange/k/cs/get-account-details",
-  "KYC_UPDATE"      : "https://xapi.kryptono.exchange/portal/account/kyc/updatestatus"
+  "KYC_UPDATE"      : "https://portal.kryptono.exchange/portal/account/kyc/updatestatus"
 };
 
 module.exports = {
