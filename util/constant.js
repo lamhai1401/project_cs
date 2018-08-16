9172983bbed5e3e14c80c1869eab68cd21dba9b1
lamhai1401 <lamthanhhai141@gmail.com>
2018-08-16 19:45:28 +0700

    fix api path

--- util/constant.js
+++ util/constant.js
@@ -3,8 +3,8 @@ const KRYPTONO_URL = {
   "RESET_SMS"               : "https://xapi.kryptono.exchange/k/cs/reset-sms",
   "LOGIN_HISTORIES"         : "https://xapi.kryptono.exchange/k/cs/login-history",
   "ACCOUNT_DETAIL"          : "https://xapi.kryptono.exchange/k/cs/get-account-details",
-  "KYC_UPDATE_APPROVE"      : "https://testenv1.kryptono.exchange/k/cs/kyc/approve",
-  "KYC_UPDATE_REJECT"       : "https://testenv1.kryptono.exchange/k/cs/kyc/reject",
+  "KYC_UPDATE_APPROVE"      : "https://xapi.kryptono.exchange/k/cs/kyc/approve",
+  "KYC_UPDATE_REJECT"       : "https://xapi.kryptono.exchange/k/cs/kyc/reject",
   "LOGIN"                   : "https://portal.kryptono.exchange/portal/j_spring_security_check",
 };
 
